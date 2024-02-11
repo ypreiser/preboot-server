5344da89eeacfa6bd6ea5b5473bdf14dfb7afdf4
ypreiser <ypreiser@gmail.com>
2024-02-11 11:35:13 +0200

    env

--- DL/db.js
+++ DL/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const MONGO_URL='mongodb+srv://test:1234@cluster0.onb7tvx.mongodb.net/yehoshua?retryWrites=true&w=majority'
+const MONGO_URL = process.env.MONGO_URL
 async function connect() {
     try {
         mongoose.connect(MONGO_URL)
